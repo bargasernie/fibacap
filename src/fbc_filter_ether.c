34550aff9563aa0700d21f2d572657c8645da511
chhquan <chhquan@foxmail.com>
2013-11-22 18:45:01 +0800

    modify src in dstaddr, and print debug messages

--- src/fbc_filter_ether.c
+++ src/fbc_filter_ether.c
@@ -4,11 +4,13 @@
 int fbc_filter_ether_srcaddr(fbc_Packet *packet, fbc_filter_arg_t arg, int arg_size)
 {
 	void *srcaddr = (Byte *)(((struct ether_header *)packet->header)->ether_shost);
-	return (memcpy(srcaddr, arg, arg_size) == 0);
+	DPRINTF("-DEBUG- fbc_filter_ether_srcaddr:\tmatching ether srcaddr\n");
+	return (memcmp(srcaddr, arg, arg_size) == 0);
 }
 
 int fbc_filter_ether_dstaddr(fbc_Packet *packet, fbc_filter_arg_t arg, int arg_size)
 {
-	void *srcaddr = (Byte *)(((struct ether_header *)packet->header)->ether_dhost);
-	return (memcpy(srcaddr, arg, arg_size) == 0);
+	void *dstaddr = (Byte *)(((struct ether_header *)packet->header)->ether_dhost);
+	DPRINTF("-DEBUG- fbc_filter_ether_dstaddr:\tmatching ether dstaddr\n");
+	return (memcmp(dstaddr, arg, arg_size) == 0);
 }
